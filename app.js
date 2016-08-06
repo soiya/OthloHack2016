e9f6a567dc03b6767644f9216801d8897df41cf6
c0rtashma <c0rtashma@gmail.com>
2016-08-07 02:27:45 +0900

    merge stylesheet

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var express = require('express')
   , http = require('http')
   , path = require('path')
   , io = require('socket.io')
- 
+
 var app = express()
   , server = require('http').createServer(app)
   , io = io.listen(server);
- 
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -19,17 +19,17 @@ app.configure(function(){
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
- 
+
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
- 
+
 server.listen(app.get('port'))
- 
-io.sockets.on('connection', function(socket) 
+
+io.sockets.on('connection', function(socket)
 {
   //socket.on(eventname, callback) でイベントを検知(=データの受信)を行います。
-  socket.on('message:send', function(data) 
+  socket.on('message:send', function(data)
   {
     //自分を含む全員宛てにメッセージを送信します。通常のチャットの発言に使える処理です。
     io.sockets.emit('message:receive', { message: data.message });
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -4,17 +4,20 @@ var socket = io.connect();
 //コンストラクタみたいなやつ
 socket.on('connect',function() {
   console.log('サーバーと接続しました。');
-  
 });
- 
+
 //socket.on(eventname, callback) でイベントを検知(=データの受信)を行います。
-//コメント受信で表示させている
+//コメントを受信して表示
 socket.on('message:receive', function (data) {
-  $("div#chat-area").prepend("<div>" + data.message + "</div>");
+  $("div#chat-area>ul#comments").prepend('<li>' + data.message + '<i class="fa fa-star-o"></i></li>');
 });
- 
+
+//コメントの送信
 function send() {
+	//入力窓の値を取得
   var msg = $("input#message").val();
+	//入力窓をリセット
   $("input#message").val("");
+	//取得した値を送信
   socket.emit('message:send', { message: msg });
 }
